ba81484d38a464e15f6c35b2961509e6ff56e334
metablog <khu10735@naver.com>
2024-01-02 22:24:40 +0900

    first-serrver

--- index.js
+++ index.js
@@ -1,38 +1,26 @@
-// import uuid from "uuid";
-const uuid = require("uuid");
+const http = require("http");
 
-class User {
-  constructor() {
-    this.db = [];
-  }
-  addUser(record) {
-    const newRecord = { ...record, _id: uuid.v4() };
-    this.db = [...this.db, newRecord];
-    return newRecord;
-  }
-  delteUser(_id) {
-    if (!_id) return `id mavjud emas`;
-    this.db = this.db.filter((v) => v._id !== _id);
-    return 1;
-  }
-  updateUser(_id, oldRecord) {
-    if (!_id) return `id mavjud emas`;
-    this.db = this.db.map((v) => (v._id === _id ? oldRecord : v));
-    return 1;
-  }
-  sayHi() {}
-}
-const user = new User();
-const resultRecord = user.addUser({
+const user = {
   name: "husan",
   surname: "butaev",
-  age: 48,
-  email: "husa@naver.com",
-  phone: "0104444445",
+  city: "soul",
+};
+
+const server = http.createServer((req, res) => {
+  const url = req.url;
+  const method = req.method;
+
+  if (url === "/" && method === "GET") {
+    res.writeHead(200);
+    return res.end("salom");
+  } else if (url === "/user" && method === "GET") {
+    res.writeHead(200, {
+      "Content-Type": "application/json",
+    });
+    return res.end(JSON.stringify(user));
+  }
 });
 
-// console.log(user.delteUser(resultRecord._id));
-console.log(
-  user.updateUser(resultRecord._id, { ...resultRecord, name: "Asadbek" })
-);
-console.log(user.db);
+server.listen(8080, () => {
+  console.log("8080 port");
+});
