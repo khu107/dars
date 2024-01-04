0d500142d535ac85ffcdc98933fd15cdf298d13f
metablog <khu10735@naver.com>
2024-01-04 19:10:21 +0900

    POST create

--- index.js
+++ index.js
@@ -1,23 +1,61 @@
 const http = require("http");
+const uuid = require("uuid");
+const users = [
+  {
+    id: 1,
+    name: "husan",
+    surname: "butaev",
+    city: "soul",
+  },
+];
 
-const user = {
-  name: "husan",
-  surname: "butaev",
-  city: "soul",
+// GET - done
+// POST
+// UPDATE
+// DELETE
+
+const retriBody = (req) => {
+  return new Promise((resolve, rejects) => {
+    try {
+      let body = "";
+      req.on("data", (chunk) => {
+        body += chunk.toString();
+      });
+
+      req.on("end", () => {
+        resolve(body);
+      });
+    } catch (error) {
+      rejects(error);
+    }
+  });
 };
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
   const url = req.url;
   const method = req.method;
 
   if (url === "/" && method === "GET") {
     res.writeHead(200);
-    return res.end("salom");
-  } else if (url === "/user" && method === "GET") {
+    return res.end("first server");
+  } else if (url === "/users" && method === "GET") {
     res.writeHead(200, {
       "Content-Type": "application/json",
     });
-    return res.end(JSON.stringify(user));
+    return res.end(JSON.stringify(users));
+  } else if (url === "/user" && method === "POST") {
+    const data = await retriBody(req);
+
+    const createdData = {
+      ...JSON.parse(data),
+      id: uuid.v4(),
+    };
+    users.push(createdData);
+    res.writeHead(201, {
+      "Content-Type": "application/json",
+    });
+
+    res.end(JSON.stringify(users));
   }
 });
 
