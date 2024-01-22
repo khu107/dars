72ca1c5a45c12939d6b577fd340aab2be7259487
metablog <khu10735@naver.com>
2024-01-23 04:13:47 +0900

    blog page 구현

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const path = require("path");
+const home = require("./home");
+const blog = require("./blog");
 const app = express();
 
 app.set("views", path.join(__dirname, "views"));
@@ -7,49 +9,11 @@ app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-  res.render("index", {
-    title: "Blog",
-    navbar: ["Works", "Blog", "Contact"],
-    posts: [
-      {
-        title: "Making a design system from scratch",
-        date: "12 Feb 2020",
-        skill: "Design, Pattern",
-        desc: "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.",
-      },
-      {
-        title: "Creating pixel perfect icons in Figma",
-        date: "12 Feb 2020",
-        skill: "figma, icon, Design",
-        desc: "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.",
-      },
-    ],
-    featWorks: [
-      {
-        src: "/image/works1.png",
-        title: "Designing Dashboards",
-        date: "2020",
-        text: "Dashboard",
-        desc: "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.",
-      },
-      {
-        src: "/image/works2.png",
-        title: "Vibrant Portraits of 2020",
-        date: "2020",
-        text: "Illustration",
-        desc: "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.",
-      },
-      {
-        src: "/image/works3.png",
-        title: "36 Days of Malayalam type",
-        date: "2018",
-        text: "Typography",
-        desc: "Amet minim mollit non deserunt ullamco  st sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.",
-      },
-    ],
-  });
+  res.render("index", { home });
 });
-
+app.get("/blog", (req, res) => {
+  res.render("blog", { blog });
+});
 app.listen(8080, () => {
   console.log("run server");
 });
