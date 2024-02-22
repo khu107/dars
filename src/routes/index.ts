966faa1835fd69411199de5dbd3019b68ae8f203
metablog <khu10735@naver.com>
2024-02-23 04:30:17 +0900

    multer 구현

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,67 @@
-import { Response, Router, Request } from "express";
+import { Request, Response, Router } from "express";
+import multer, { StorageEngine, diskStorage } from "multer";
+import path from "path";
+import imageModel, { IImage } from "../schema/image.schema";
+import { v2 } from "cloudinary";
+import fs from "fs/promises";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+const storage: StorageEngine = diskStorage({
+  destination: (req: Request, file: Express.Multer.File, cb) => {
+    cb(null, "uploads/");
+  },
+  filename: (req: Request, file: Express.Multer.File, cb) => {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    const fileExt = path.extname(file.originalname);
+    const filename = `file-${uniqueSuffix}${fileExt}`;
+    cb(null, filename);
+  },
+});
+// setup multer
+const upload = multer({ storage });
+
+const config = v2.config({
+  cloud_name: process.env.cloudName,
+  api_key: process.env.apiKey,
+  api_secret: process.env.apiSecret,
+});
 
 const router = Router();
 
-router.get("/", (req: Request, res: Response) => {
-  res.status(200).json({ message: "succsess" });
+router.get("/", async (req: Request, res: Response) => {
+  return res.status(200).json({
+    message: "success",
+    data: await imageModel.find<IImage[]>(),
+  });
 });
+
+router.post("/", upload.single("file"), async (req: Request, res: Response) => {
+  const uploadFile = await v2.uploader.upload(
+    String(req.file?.path),
+    (err, resp) => {
+      if (err) {
+        console.log(err);
+        return res.status(500).json({
+          success: false,
+          message: "error",
+        });
+      }
+      const createdData = imageModel.create<IImage>({
+        title: String(req.file?.originalname),
+        image_source: String(resp?.secure_url),
+        description: String(req.file?.originalname),
+      });
+    }
+  );
+
+  fs.unlink(String(req.file?.path))
+    .then(() => console.log("image uchirildi"))
+    .catch((err) => console.error(err));
+  return res.status(200).json({
+    message: "success",
+    file: uploadFile,
+  });
+});
+
 export default router;
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,31 @@
-import express, { Application, NextFunction, Request, Response } from "express";
+import * as dotenv from "dotenv";
+dotenv.config();
+import express, { Application } from "express";
+import mongoose from "mongoose";
 import cors from "cors";
 import helmet from "helmet";
-import router from "./routes";
+import image_router from "./routes";
+
 const app: Application = express();
 
 app.use(cors());
 app.use(helmet());
-app.use(router);
+app.use(express.json());
+app.use("/uploads", express.static("uploads"));
 
-app.listen(8080, () => {
-  console.log(`run server http//localhost:${8080}`);
+app.use("/image", image_router);
+
+app.listen(8080, async (): Promise<void> => {
+  try {
+    const dbUri = process.env.DB_URI;
+    if (!dbUri) {
+      throw new Error("DB_URI environment variable is not defined.");
+    }
+    await mongoose.connect(dbUri);
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+    throw error; // propagate the error to the calling code
+  }
+  console.log("server run");
 });
